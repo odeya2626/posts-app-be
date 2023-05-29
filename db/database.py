d5d69694e34a873a58c399cb497ff13aa2428672
Odeya <odeyacoh26@gmail.com>
2023-05-29 13:24:17 +0300

    upload db

--- db/database.py
+++ db/database.py
@@ -1,12 +1,14 @@
+import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./db.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://icuafboo:dxh8xFRwo3_MfMfx4JOLJwnmQ0R9Rnmb@lucky.db.elephantsql.com/icuafboo"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
